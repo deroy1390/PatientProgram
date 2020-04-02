3dea361a026edd7881e06df681acd54ff8511d90
Ethan DeRoy <ethanderoy@gmail.com>
2020-04-02 12:11:09 -0500

    Added Database Activity

--- src/patientprogram/PatientProgram.java
+++ src/patientprogram/PatientProgram.java
@@ -2,6 +2,9 @@ package patientprogram;
 import java.util.*;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,46 +18,65 @@ public class PatientProgram
      */
     public static void main(String[] args) 
     {
-        Scanner in = new Scanner(System.in);
-        boolean on = true;
-        
-        while(on)
+        try
         {
-            String selection = JOptionPane.showInputDialog("########Welcome to the Patient Tracking System########\n"
-       + "Select 1 to add a patient\n");
+            String host = "jdbc:derby://localhost:1527/contact";
+            String uName = "nbuser";
+            String uPass = "nbuser";
             
-            if(selection.equals("1"))
+            Connection con = DriverManager.getConnection(host, uName, uPass);
+            
+            Scanner in = new Scanner(System.in);
+            boolean on = true;
+        
+            while(on)
             {
-                String patientFirst = JOptionPane.showInputDialog("####Enter New Patient Information####\n"
-                + "Enter Patient First Name: ");
-                String patientLast = JOptionPane.showInputDialog("Enter Patient Last Name: ");
-                String patientAge = JOptionPane.showInputDialog("Enter Patient Age: ");
-                String patientPhone = JOptionPane.showInputDialog("Enter Patient Phone: ");
-                String patientAddress = JOptionPane.showInputDialog("Enter Patient Address: ");
-                
-                AddPatient addPatient = new AddPatient();
-                addPatient.patientAdd(patientFirst, patientLast, patientAge, patientPhone, patientAddress);
-                String verify = JOptionPane.showInputDialog("Is the following information correct?\n"
-                + "Patient Name: " + addPatient.patientFirst + " " + addPatient.patientLast + "\n"
-                + "Patient Age: " + addPatient.patientAge + "\n"
-                + "Patient Phone: " + addPatient.patientPhone + "\n"
-                + "Patient Address: " + addPatient.patientAddress
-                + "Yes/No");
-                
-         
-                if(verify.equalsIgnoreCase("Yes"))
+                String selection = JOptionPane.showInputDialog("########Welcome to the Patient Tracking System########\n"
+           + "Select 1 to add a patient\n");
+
+                if(selection.equals("1"))
                 {
-                    JFrame f = null;
-                   
-                        JOptionPane.showMessageDialog(f, "Patient Added");
-                         on = false;   
+                    String patientFirst = JOptionPane.showInputDialog("####Enter New Patient Information####\n"
+                    + "Enter Patient First Name: ");
+                    String patientLast = JOptionPane.showInputDialog("Enter Patient Last Name: ");
+                    String patientAge = JOptionPane.showInputDialog("Enter Patient Age: ");
+                    String patientPhone = JOptionPane.showInputDialog("Enter Patient Phone: ");
+                    String patientAddress = JOptionPane.showInputDialog("Enter Patient Address: ");
+
+                    AddPatient addPatient = new AddPatient();
+                    addPatient.patientAdd(patientFirst, patientLast, patientAge, patientPhone, patientAddress);
+                    String verify = JOptionPane.showInputDialog("Is the following information correct?\n"
+                    + "Patient Name: " + addPatient.patientFirst + " " + addPatient.patientLast + "\n"
+                    + "Patient Age: " + addPatient.patientAge + "\n"
+                    + "Patient Phone: " + addPatient.patientPhone + "\n"
+                    + "Patient Address: " + addPatient.patientAddress
+                    + "Yes/No");
+
+
+                    if(verify.equalsIgnoreCase("Yes"))
+                    {
+                        JFrame f = null;
+
+                            JOptionPane.showMessageDialog(f, "Patient Added");
+                             on = false;   
+                    }
+
+
+
                 }
-                
-                
-                
+
             }
-            
+
+
+
+            }
+        
+        catch(SQLException err)
+        {
+            JFrame z = null;
+            JOptionPane.showMessageDialog(z,err.getMessage());
         }
+        
         
         
         
