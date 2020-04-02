e4256289d96e8d6e1110321a452b9d192ad9af50
Ethan DeRoy <ethanderoy@gmail.com>
2020-04-01 20:28:32 -0500

    First Commit

---
+++ src/patientprogram/PatientProgram.java
@@ -0,0 +1,19 @@
+package patientprogram;
+import java.util.*;
+
+/**
+ *
+ * @author Ethan DeRoy
+ */
+public class PatientProgram 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+    }
+    
+}
